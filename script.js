8935e7f6863e60dc7de627a446b33826762546b7
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-18 11:01:58 -0700

    New changes

---
+++ script.js
@@ -0,0 +1,4 @@
+const addTodoBtn = document.querySelector("#add-new-todo-btn");
+const newTodoInput = document.querySelector("#new-todo-input");
+const ulTodoList = document.querySelector("main ul");
+const clearAllTodosBtn = document.querySelector("footer button");
