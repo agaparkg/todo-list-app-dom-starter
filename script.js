cd121d8dad0981218a7d61f537c4055d428cb7d4
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-18 17:49:26 -0700

    new changes

--- script.js
+++ script.js
@@ -17,6 +17,26 @@ function getDataFromLocalStorage() {
 
 let todos = getDataFromLocalStorage(); // []
 
+clearAllTodosBtn.addEventListener("click", () => {
+  todos = [];
+  // todos.length = 0
+  updateLocalStorage();
+  generateTodoListView();
+});
+
+// [
+//   {
+//     id: 1,
+//     text: "Todo 1",
+//     completed: false,
+//   },
+//   {
+//     id: 2,
+//     text: "Todo 2",
+//     completed: false,
+//   },
+// ];
+
 function generateSingleTodoItem(todo) {
   const todoItem = `
         <div class="input-group mb-1">
@@ -26,11 +46,38 @@ function generateSingleTodoItem(todo) {
               class="form-control"
               value="${todo.text}"
             />
-            <button type="button" class="btn btn-danger">Delete</button>
+            <button onclick="deleteTodo(${todo.id})" type="button" class="btn btn-danger">Delete</button>
           </div>
     `;
 
   ulTodoList.innerHTML += todoItem;
+}
+
+function deleteTodo(id) {
+  // Opt 1
+  const newTodos = [];
+
+  for (const todo of todos) {
+    if (todo.id !== id) {
+      newTodos.push(todo);
+    }
+  }
+
+  todos = newTodos;
+
+  // Opt 2
+  //   let index;
+  //   for (let i = 0; i < todos.length; i++) {
+  //     if (todos[i].id === id) {
+  //       // 1 === 3
+  //       index = i;
+  //     }
+  //   }
+
+  //   todos.splice(index, 1); // [1,2,4,5,6]
+
+  updateLocalStorage();
+  generateTodoListView();
 }
 
 function generateTodoListView() {
@@ -51,16 +98,21 @@ function addNewTodoFn() {
 
   if (inputValue !== "") {
     const newTodo = {
-      id: "",
+      id: todos.length ? todos[todos.length - 1].id + 1 : 1,
       text: inputValue,
       completed: false,
     };
 
     todos.push(newTodo);
+    updateLocalStorage();
     generateTodoListView();
   } else {
     alert("Please enter a text.");
   }
 
   newTodoInput.value = "";
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
